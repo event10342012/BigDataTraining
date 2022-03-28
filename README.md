154dac1fe532425a1a6a28544f70b41ee7ef6ad2
leochen <event10342012@gmail.com>
2022-03-28 23:36:48 +0800

    feat: hbase homework

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # BigDataTraining
 
 
-## Hbase homework
+### W2 Homework - MapReduce
 
+
+###W3 homework - HBase
+
+1. create namespace
 ```shell
 create_namespace '陳宏育'
 ```
+
+```shell
+
+```
+
--- src/main/java/HbaseOperator.java
+++ src/main/java/HbaseOperator.java
@@ -5,7 +5,9 @@ import org.apache.hadoop.hbase.util.Bytes;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class HbaseOperator {
     private static Connection connection;
@@ -94,10 +96,24 @@ public class HbaseOperator {
         List<String> columnFamilies = Arrays.asList("info", "score");
         String tableName = "陳宏育:student";
         HbaseOperator.createTable(tableName, columnFamilies);
-        HbaseOperator.putData(tableName, "陳宏育", "info", "student_id", "G20220735020143");
-        HbaseOperator.putData(tableName, "陳宏育", "info", "class", "1");
-        HbaseOperator.putData(tableName, "陳宏育", "score", "understanding", "80");
-        HbaseOperator.putData(tableName, "陳宏育", "score", "programming", "80");
+
+        Map<String, List<Long>> dataMap = new HashMap<>();
+        dataMap.put("Tom", Arrays.asList(20210000000001L, 1L, 75L, 82L));
+        dataMap.put("Jerry", Arrays.asList(20210000000002L, 1L, 85L, 67L));
+        dataMap.put("Jack", Arrays.asList(20210000000003L, 2L, 80L, 80L));
+        dataMap.put("Rose", Arrays.asList(20210000000004L, 2L, 60L, 61L));
+        dataMap.put("陳宏育", Arrays.asList(20220735020143L, 1L, 80L, 80L));
+
+        dataMap.forEach((k, v) -> {
+            try{
+                putData(tableName, k, "info", "sutdent_id", v.get(0).toString());
+                putData(tableName, k, "info", "class", v.get(1).toString());
+                putData(tableName, k, "score", "understanding", v.get(2).toString());
+                putData(tableName, k, "score", "programming", v.get(3).toString());
+            } catch (Exception e){
+                System.out.println(e);
+            }
+        });
 
         HbaseOperator.scanTable(tableName);
     }
